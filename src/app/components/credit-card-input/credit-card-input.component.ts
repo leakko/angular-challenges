f7df07817f29b4630a661b71b5a65309b3d066d7
Marcos Rodriguez <marcos.rodriguez@guadaltech.es>
2022-01-14 12:23:17 +0100

    [COMP] Credit card input finished

--- src/app/components/credit-card-input/credit-card-input.component.ts
+++ src/app/components/credit-card-input/credit-card-input.component.ts
@@ -12,12 +12,21 @@ export class CreditCardInputComponent implements OnInit {
     return this.readonly
   }
   @Input() public set isReadonly(value: boolean) {
-    this.creditCardNumber = value ? 
+    this.creditCardNumber = value ? this.formatReadonlyCCNum(this.creditCardNumber) : this.creditCardNumber;
     this.readonly = value;
   }
 
   private formatReadonlyCCNum(ccNum: string): string {
     const parts = ccNum.match(/[\s\S]{1,4}/g) || [];
+    const onlyLast4Shown = parts.map((part, index) => {
+      if(index == parts.length - 1) {
+        return part
+      }
+
+      return "xxxx";
+    })
+
+    return onlyLast4Shown.join("-")
   }
 
   constructor() { }
